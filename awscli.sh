c6c2a176e6a036c0df1b4fb84eef3872abc6253b
Jason Martin <jhmartin@toger.us>
2016-04-30 21:02:56 -0700

    Create awscli.sh
    Add shell script for non-Cloudformation components

---
+++ awscli.sh
@@ -0,0 +1,2 @@
+# 1.5 - 1.11
+aws iam update-account-password-policy  --require-uppercase-characters  --require-symbols --require-uppercase-characters --require-lowercase-characters --minimum-password-length 14 --password-reuse-prevention 24 --max-password-age 90
